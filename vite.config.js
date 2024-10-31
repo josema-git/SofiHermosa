5b314e94001b7762496efca6f95497861f1e6dff
josema-git <joseman6575@gmail.com>
2024-10-31 17:26:21 -0500

    fix: update build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         manualChunks: undefined,
-      },
-    },
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 3000,
-    strictPort: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'text/javascript',
-    },
-  },
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
