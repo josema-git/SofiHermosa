5174e1f8aba9872e61d05367489e09eed4fe8ba3
josema-git <joseman6575@gmail.com>
2024-10-31 17:23:41 -0500

    fix: add netlify configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/javascript',
     },
   },
 })
